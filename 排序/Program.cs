37d76b169ab2487094cefd6e85e2faf275f59993
hans <545872067@qq.com>
2018-09-11 11:31:29 +0800

    添加项目文件。

---
+++ 排序/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 顺序查找1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> {2,3,6,1,9,7 };
+
+            var result = SequeceSearch(list, 5);
+
+            if(result!=-1)
+            {
+                Console.WriteLine("5 在数组中找到， 索引的位置是,{0}",result);
+            }
+            else
+            {
+                Console.WriteLine("在数组中 没有");
+            }
+            Console.ReadLine();
+
+        }
+        /// <summary>
+        /// 顺序查找
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="Key"></param>
+        /// <returns></returns>
+        static int SequeceSearch(List<int> list,int Key)
+        {
+            for(int i=0;i<list.Count;i++)
+            {
+                //查找成功，返回序列号
+                if(Key==list[i])
+                {
+                    return i;
+                }
+            }
+            //查找失败返回-1
+            return -1;
+        }
+    }
+}
